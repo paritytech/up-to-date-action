aba414bc6145c3218340b35fddc015d04a68d7f9
Javier Bullrich <javier@bullrich.dev>
2024-01-05 15:57:53 -0300

    added code to update the prs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { getInput, info, setOutput } from "@actions/core";
+import { getInput, setOutput, summary } from "@actions/core";
+import { SummaryTableRow } from "@actions/core/lib/summary";
 import { context, getOctokit } from "@actions/github";
 import { Context } from "@actions/github/lib/context";
-import { PullRequest } from "@octokit/webhooks-types";
 
 import { PullRequestApi } from "./github/pullRequest";
 import { generateCoreLogger } from "./util";
@@ -23,10 +23,47 @@ const getRepo = (ctx: Context) => {
 const repo = getRepo(context);
 
 setOutput("repo", `${repo.owner}/${repo.repo}`);
+const logger = generateCoreLogger();
 
-if (context.payload.pull_request) {
+const action = async () => {
   const token = getInput("GITHUB_TOKEN", { required: true });
-  const api = new PullRequestApi(getOctokit(token), generateCoreLogger());
-  const author = api.getPrAuthor(context.payload.pull_request as PullRequest);
-  info("Author of the PR is " + author);
-}
+  const repoInfo = getRepo(context);
+  const api = new PullRequestApi(getOctokit(token), logger, repoInfo);
+  const prs = await api.listPRs(true);
+  if (prs.length > 0) {
+    logger.info(`About to update ${prs.length} PRs 🗄️`);
+    const rows: SummaryTableRow[] = [
+      [
+        { data: "PR Number", header: true },
+        { data: "Result", header: true },
+      ],
+    ];
+    for (const number of prs) {
+      logger.info(`📡 - Updating #${number}`);
+      const repoTxt = `[#${number}](https://github.com/${repoInfo.owner}/${repoInfo.repo}/pull/${number})`;
+      try {
+        await api.update(number);
+        rows.push([repoTxt, "Pass ✅"]);
+        logger.info(`📥 - Updated #${number}`);
+      } catch (error) {
+        logger.error(error as string | Error);
+        rows.push([repoTxt, "Fail ❌"]);
+      }
+    }
+    logger.info(" - Finished updating PRs");
+    await summary
+      .addHeading("Up to date", 1)
+      .addHeading("PRs updated", 3)
+      .addTable(rows)
+      .write();
+  } else {
+    logger.info("No matching PRs found. Aborting");
+    summary.addHeading("Up to date", 1).addHeading("No matching PRs found");
+  }
+};
+
+action()
+  .then(() => {
+    logger.info("Operation completed");
+  })
+  .catch((e) => logger.error(e as Error));
