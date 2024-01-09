83c26a5436568a5a4ceaa1ccf271697228613a4d
Javier Bullrich <javier@bullrich.dev>
2024-01-09 11:41:07 -0300

    Created base action (#5)
    Created base action with the ability to update PRs and print a summary
    of the results.
    
    Resolves #2

--- src/github/pullRequest.ts
+++ src/github/pullRequest.ts
@@ -1,15 +1,35 @@
-import { PullRequest } from "@octokit/webhooks-types";
-
-import { ActionLogger, GitHubClient } from "./types";
+import { GitHubClient } from "./types";
 
 /** API class that uses the default token to access the data from the pull request and the repository */
 export class PullRequestApi {
   constructor(
     private readonly api: GitHubClient,
-    private readonly logger: ActionLogger,
+    private readonly repo: { owner: string; repo: string },
   ) {}
 
-  getPrAuthor(pr: PullRequest): string {
-    return pr.user.login;
+  async listPRs(
+    onlyAutoMerge: boolean,
+  ): Promise<{ number: number; title: string }[]> {
+    const openPRs = await this.api.paginate(this.api.rest.pulls.list, {
+      ...this.repo,
+      state: "open",
+    });
+
+    if (onlyAutoMerge) {
+      return openPRs
+        .filter((pr) => pr.auto_merge)
+        .map(({ number, title }) => ({ number, title }) as const);
+    } else {
+      return openPRs.map(({ number, title }) => ({ number, title }) as const);
+    }
+  }
+
+  async update(number: number): Promise<string | undefined> {
+    const { data } = await this.api.rest.pulls.updateBranch({
+      ...this.repo,
+      pull_number: number,
+    });
+
+    return data.message;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { getInput, info, setOutput } from "@actions/core";
+import { getInput, setFailed, setOutput, summary } from "@actions/core";
+import { SummaryTableRow } from "@actions/core/lib/summary";
 import { context, getOctokit } from "@actions/github";
 import { Context } from "@actions/github/lib/context";
-import { PullRequest } from "@octokit/webhooks-types";
 
 import { PullRequestApi } from "./github/pullRequest";
 import { generateCoreLogger } from "./util";
@@ -23,10 +23,48 @@ const getRepo = (ctx: Context) => {
 const repo = getRepo(context);
 
 setOutput("repo", `${repo.owner}/${repo.repo}`);
+const logger = generateCoreLogger();
 
-if (context.payload.pull_request) {
+const action = async () => {
   const token = getInput("GITHUB_TOKEN", { required: true });
-  const api = new PullRequestApi(getOctokit(token), generateCoreLogger());
-  const author = api.getPrAuthor(context.payload.pull_request as PullRequest);
-  info("Author of the PR is " + author);
-}
+  const repoInfo = getRepo(context);
+  const api = new PullRequestApi(getOctokit(token), repoInfo);
+  const prs = await api.listPRs(false);
+  if (prs.length > 0) {
+    logger.info(`About to update ${prs.length} PRs 🗄️`);
+    const rows: SummaryTableRow[] = [
+      [
+        { data: "PR Number", header: true },
+        { data: "Title", header: true },
+        { data: "Result", header: true },
+      ],
+    ];
+    for (const { number, title } of prs.sort((a, b) => a.number - b.number)) {
+      logger.info(`📡 - Updating '${title}' #${number}`);
+      const repoTxt = `${repoInfo.owner}/${repoInfo.repo}#${number}`;
+      try {
+        await api.update(number);
+        rows.push([repoTxt, title, "Pass ✅"]);
+        logger.info(`📥 - Updated #${number}`);
+      } catch (error) {
+        logger.error(error as string | Error);
+        rows.push([repoTxt, title, "Fail ❌"]);
+      }
+    }
+    logger.info("🪄 - Finished updating PRs");
+    await summary
+      .addHeading("Up to date", 1)
+      .addHeading("PRs updated", 3)
+      .addTable(rows)
+      .write();
+  } else {
+    logger.info("No matching PRs found. Aborting");
+    summary.addHeading("Up to date", 1).addHeading("No matching PRs found");
+  }
+};
+
+action()
+  .then(() => {
+    logger.info("Operation completed");
+  })
+  .catch(setFailed);
